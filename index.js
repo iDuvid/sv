f8962f1e4bf9f0a72db31067f989bcfa6884305d
David <84334203+iDuvid@users.noreply.github.com>
2024-06-27 14:53:26 +0200

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-test
+const express = require('express');
+const app = express();
+
+const PORT = 25565;  // Puerto especificado
+
+app.get('*', (req, res) => {
+    res.redirect('https://iduvid.github.io' + req.originalUrl);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor de redirección escuchando en el puerto ${PORT}`);
+});
